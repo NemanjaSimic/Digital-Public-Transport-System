ec2fc0a31180e6c2cee04a424fca42914da05a60
NemanjaSimic <snemanja.simic@gmail.com>
2019-06-03 13:32:19 +0200

    Connecting backend and frontend

---
+++ Backend/WebApp/Models/Karta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Karta
+    {
+        [Key]
+        public int ID { get; set; }
+        public bool Validna { get; set; }
+        public DateTime DatumIzdavanja { get; set; }
+        public int StavkaCenovnikaId { get; set; }
+        public StavkaCenovnika StavkaCenovnika { get; set; }
+    }
+}
---
+++ Backend/WebApp/Models/Koordinata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Koordinata
+    {
+        [Key]
+        public int Id { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
